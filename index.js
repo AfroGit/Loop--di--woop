5f6ae08c4fecb20e5f81c88c4aae7f8ef55a9c4c
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-20 23:25:51 +0000

    hover looks good on buttons

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const myArray = [6, 19, 20];
-const yourArray = [19, 81, 2];
-for (let i = 0; i < myArray.length; i++) {
-  for (let j = 0; j < yourArray.length; j++) {
-    if (myArray[i] === yourArray[j]) {
-      // console.log('Both loops have the number: ' + yourArray[j])
-    }
-  }
-};
+// const myArray = [6, 19, 20];
+// const yourArray = [19, 81, 2];
+// for (let i = 0; i < myArray.length; i++) {
+//   for (let j = 0; j < yourArray.length; j++) {
+//     if (myArray[i] === yourArray[j]) {
+//       // console.log('Both loops have the number: ' + yourArray[j])
+//     }
+//   }
+// };
 
-const bobsFollowers = ['Dill', 'Flo', 'Milli', 'Bukk'];
-const tinasFollowers = ['Dreko', 'Bukk', 'Flo'];
-const  mutualFollowers = [];
-for(let i = 0; i < bobsFollowers.length; i++){
-    for(let f = 0; f < tinasFollowers.length; f++){
-      if(bobsFollowers[i] === tinasFollowers[f]){
-       mutualFollowers.push(bobsFollowers[i]);//used the push method to insert the common data found in both arrays.
-      }
-    }
-};
- console.log(mutualFollowers);
+// const bobsFollowers = ['Dill', 'Flo', 'Milli', 'Bukk'];
+// const tinasFollowers = ['Dreko', 'Bukk', 'Flo'];
+// const  mutualFollowers = [];
+// for(let i = 0; i < bobsFollowers.length; i++){
+//     for(let f = 0; f < tinasFollowers.length; f++){
+//       if(bobsFollowers[i] === tinasFollowers[f]){
+//        mutualFollowers.push(bobsFollowers[i]);//used the push method to insert the common data found in both arrays.
+//       }
+//     }
+// };
+//  console.log(mutualFollowers);
